e5791245d824a496940e1191a41c3919f0c31dc5
John A. Joyce <48220338+jajoyce@users.noreply.github.com>
2022-02-23 17:47:32 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import weatherData from "./weatherData";
+console.log("imported weatherData:", weatherData);
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>My WeatherIcons App</h1>
+    </div>
+  );
+}
---
+++ src/weatherData.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg",
+    conditions: "Sunny",
+    time: "Day"
+  },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg",
+    conditions: "Clear",
+    time: "Night"
+  },
+  {
+    img:
+      "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg",
+    conditions: "Thuderstorms",
+    time: "Day"
+  },
+  {
+    img:
+      "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg",
+    conditions: "Partly Cloudy",
+    time: "Night"
+  },
+  {
+    img:
+      "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg",
+    conditions: "Mostly Sunny",
+    time: "Day"
+  }
+];
