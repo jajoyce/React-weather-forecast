7f731839cfd5f794e02c56359d3b55fc4ce581a7
John A. Joyce <48220338+jajoyce@users.noreply.github.com>
2022-02-23 19:09:32 -0500

    Create WeatherForecast.js component. Create WeatherForecast.js component, set up to accept props and update JSX. Complete parts 1 and 2 of deliverable.

---
+++ src/components/WeatherForecast.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const WeatherForecast = (props) => {
+  return (
+    <div class="weather">
+      <img src={props.img} alt="Weather icon" />
+      <p>
+        <span>Conditions:</span> {props.conditions}
+      </p>
+      <p>
+        <span>Time:</span> {props.time}
+      </p>
+    </div>
+  );
+};
+
+export default WeatherForecast;
